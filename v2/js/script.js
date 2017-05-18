e15e5565087f034904d0e043b7cc3c696f49010d
Daniyal Nawaz <dara.daniyal@gmail.com>
2017-05-18 23:09:55 +0500

    Changes in v2

--- v2/js/script.js
+++ v2/js/script.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
   scrollCheck();
-  countdown();
 
   // on scroll
   $(window).scroll(function(){
@@ -10,6 +9,14 @@ $(document).ready(function(){
 
   $('#hc-nav-wrapper .menu-toggle').click(function (){
     $('#hc-nav-wrapper .hc-navigation .hc-menu-container').toggle('slow');
+  });
+
+  $(document).on('click', '.learn-more', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $('#hc-why-token').offset().top - 20
+    }, 500);
   });
 });
 
@@ -23,45 +30,4 @@ function scrollCheck(){
   } else {
     $navWrapper.removeClass('add-background');
   }
-}
-
-function countdown() {
-  var countDownDate = new Date("Aug 5, 2017 15:37:25").getTime();
-
-  // Update the count down every 1 second
-  var x = setInterval(function() {
-
-    // Get todays date and time
-    var now = new Date().getTime();
-
-    // Find the distance between now an the count down date
-    var distance = countDownDate - now;
-
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    if (minutes < 10) {
-      minutes = '0' + minutes;
-    }
-
-    if (seconds < 10) {
-      seconds = '0' + seconds;
-    }
-
-    if (hours < 10) {
-      hours = '0' + hours;
-    }
-
-    // Display the result in the element with id="demo"
-    $('.countdown').html(days + ":" + hours + ":" + minutes + ":" + seconds);
-
-    // If the count down is finished, write some text
-    if (distance < 0) {
-      clearInterval(x);
-      $('.countdown').html("EXPIRED");
-    }
-  }, 1000);
 }
