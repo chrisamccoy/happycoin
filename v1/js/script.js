99130a9cc95b8ddee27dd03335c7a6c294b546e7
Aymon Nawaz <aymonnawaz123@gmail.com>
2017-06-10 01:06:20 +0500

    layout changes

--- v1/js/script.js
+++ v1/js/script.js
@@ -39,6 +39,8 @@ $(document).ready(function(){
     }
     // console.log($(this).next('.tech-desc'));
   });
+
+  renderCircles();
 });
 
 
@@ -51,4 +53,44 @@ function scrollCheck(){
   } else {
     //$navWrapper.removeClass('add-background');
   }
+}
+
+function generateCircle (element, value) {
+  element = document.getElementById(element);
+  var bar = new ProgressBar.Circle(element, {
+    color: '#5d794c',
+    // This has to be the same size as the maximum width to
+    // prevent clipping
+    strokeWidth: 2,
+    trailWidth: 4,
+    easing: 'easeInOut',
+    text: {
+      autoStyleContainer: false
+    },
+    from: { color: '#5d794c', width: 5 },
+    to: { color: '#5d794c', width: 5 },
+    // Set default step function for all animate calls
+    step: function(state, circle) {
+      circle.path.setAttribute('stroke', state.color);
+      circle.path.setAttribute('stroke-width', state.width);
+
+      var value = Math.round(circle.value() * 100);
+      if (value === 0) {
+        circle.setText('0');
+      } else {
+        circle.setText(value+"%");
+      }
+
+    }
+  });
+
+  bar.text.style.fontSize = '2rem';
+
+  bar.animate(value);
+}
+
+function renderCircles() {
+  generateCircle('circle1', 0.75);
+  generateCircle('circle2', 0.15);
+  generateCircle('circle3', 0.15);
 }
