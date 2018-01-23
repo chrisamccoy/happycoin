a2ae1f550c7467cddbffc0f21fe1e1e60766359c
Aymon Nawaz <aymonnawaz123@gmail.com>
2018-01-23 11:18:14 +0500

    layout changes

--- app/helpers/blogs.js
+++ app/helpers/blogs.js
@@ -3,7 +3,7 @@ var blogs = [
       slug: 'load-testing-storecoins-dynamic-proof-of-stake-consensus-algorithm-test-1-of-7',
       title: 'Load Testing Storecoin’s Dynamic Proof-of-Stake Consensus Algorithm: Test 1 of 7',
       desc: 'Testing the impact of varying numbers and sizes of transactions processed on a 4-node network running the Storecoin Dynamic Proof-of-Stake Consensus Algorithm (DyPoS).',
-      img: 'https://storeco.in/images/blog-load-testing.png',
+      img: 'https://storeco.in/images/blog/blog-load-testing.png',
       template: 'blog-load-testing',
       timeStamp : 'Dec 13, 2017 . 9 min read'
   },
