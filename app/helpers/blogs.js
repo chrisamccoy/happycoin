67bdd27e5276be5427f2d805ec58cc515d46e481
Aymon Nawaz <aymonnawaz123@gmail.com>
2018-02-02 01:37:07 +0500

    layout changes

--- app/helpers/blogs.js
+++ app/helpers/blogs.js
@@ -49,8 +49,8 @@ var blogs = [
       date : 'December 13, 2017'
   },
   {
-      slug: 'storecoin-creator-chris-mccoy-to-speak-at-words-1st-crypto-finance-conference',
-      title: 'Creator Chris McCoy Introduces Storecoin at Word’s 1st Crypto Finance Conference',
+      slug: 'chris-mccoy-introduces-storecoin-at-worlds-1st-crypto-finance-conf',
+      title: 'Creator Chris McCoy Introduces Storecoin at World’s 1st Crypto Finance Conference',
       desc: 'St. MORITZ, Switzerland — Storecoin creator Chris McCoy introduced the Storecoin blockchain to an international audience at the Crypto Finance Conference 2018 in St. Moritz.',
       img: 'https://storeco.in/images/blog/chris_cfc18_preso.png',
       template: 'blog-crypto-conference',
