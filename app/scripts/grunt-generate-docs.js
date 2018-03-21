f7323bc38762b3a3d6a4f06965da768c78012f37
Daniyal Nawaz <dara.daniyal@gmail.com>
2018-03-21 15:02:53 +0500

    Changes

--- app/scripts/grunt-generate-docs.js
+++ app/scripts/grunt-generate-docs.js
@@ -1,5 +1,6 @@
-var fs = require('fs');
-var rimraf = require('rimraf');
+const fs = require('fs'),
+      rimraf = require('rimraf'),
+      MarkdownIt = require('markdown-it');
 
 function command(grunt, cmd, args) {
   return new Promise(resolve => {
@@ -34,24 +35,53 @@ function cloneRepo(grunt, repoUrl, dir) {
   });
 }
 
+function readFile(filePath) {
+  return fs.readFileSync(filePath).toString();
+}
+
+function createHtmlFile(filePath, content) {
+  const fileSplit = filePath.split('/');
+        dir = './views/developers',
+        filename = fileSplit[fileSplit.length - 1]
+                      .replace('.md', '.html')
+                      .toLowerCase();
+
+  if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+  }
+
+  fs.writeFileSync(dir + '/' + filename, content);
+}
+
+function copyAndConvert(grunt, dir) {
+  return new Promise(resolve => {
+    const md = new MarkdownIt(),
+          file = dir + '/README.md';
+
+    const doc = readFile(file);
+    createHtmlFile(file, md.render(doc))
+    resolve();
+  });
+}
+
 async function start(grunt, options) {
   const repoDir = './' + options.repoDir,
         repoDocsDir = './' + options.repoDir + '/docs';
 
-  // Check for repo directory, make one if doesn't exist
+  // STEP 1: Check for repo directory, make one if doesn't exist
   if (!fs.existsSync(repoDir)){
     grunt.log.writeln('Creating directory: ' + repoDir);
     fs.mkdirSync(repoDir);
   }
 
-  // Clone repository
+  // STEP 2:  Clone repository
   const cmd = await cloneRepo(grunt, options.repo, repoDocsDir);
 
-  // TODO: Copy directories in 'repoPath' to /views/developers
+  // TODO STEP 5: Place all directories and files in
+  // /views/developers/ and convert md files into html
+  await copyAndConvert(grunt, repoDocsDir);
 
-  // TODO: Covert Markdown (MD) files to HTML
-
-  // Remove the repo directory
+  // STEP 4: Remove the repo directory
   await removeDirectory(grunt, repoDir);
 
   // End
