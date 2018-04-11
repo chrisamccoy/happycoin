45794f3b3df94eb5956da4997441e1999203c509
Aymon Nawaz <aymonnawaz123@gmail.com>
2018-04-11 20:37:09 +0500

    laouyt changes

--- app/public/js/admin/script.js
+++ app/public/js/admin/script.js
@@ -58,24 +58,74 @@ function loaded() {
 function initCreateOrders() {
   var $createOrder = $('#create-orders');
   $form = $createOrder.find('form');
+  var country_list = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States","Uruguay","Uzbekistan","Venezuela","Vietnam","Yemen","Zambia","Zimbabwe"];
+  var us_states = ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
 
-  $('#autocomplete').autocomplete({
-    source: $.map(inventory, function(value){
-      return { label : value.name, value : value.id }
-    }),
-    focus: function(event, ui) {
-      $('#autocomplete').val(ui.item.label);
-      return false;
-    },
-    // Once a value in the drop down list is selected, do the following:
-    select: function(event, ui) {
-      // place the person.given_name value into the textfield called 'select_origin'...
-      $('#autocomplete').val(ui.item.label);
-      // and place the person.id into the hidden textfield called 'link_origin_id'.
-      $('input[name="product_id"]').val(ui.item.value);
-      return false;
+  // $('#autocomplete').autocomplete({
+  //   source: $.map(inventory, function(value){
+  //     return { label : value.name, value : value.id }
+  //   }),
+  //   focus: function(event, ui) {
+  //     $('#autocomplete').val(ui.item.label);
+  //     return false;
+  //   },
+  //   // Once a value in the drop down list is selected, do the following:
+  //   select: function(event, ui) {
+  //     // place the person.given_name value into the textfield called 'select_origin'...
+  //     $('#autocomplete').val(ui.item.label);
+  //     // and place the person.id into the hidden textfield called 'link_origin_id'.
+  //     $('input[name="product_id"]').val(ui.item.value);
+  //     return false;
+  //   }
+  // });
+
+  country_list = $.map(country_list, function(value){
+    return { id : value, name : value }
+  });
+
+  us_states = $.map(us_states, function(value){
+    return { id : value, name : value }
+  });
+
+  // console.log(country_list);
+
+  templateRender({
+    el : '#product-select',
+    data : { rows : inventory, select : 'Product Select' }
+  });
+
+  templateRender({
+    el : '#country-select',
+    data : { rows : country_list, select : 'Country Select' },
+    tpl : '#product-select-tpl'
+  });
+
+  templateRender({
+    el : '#state-select',
+    data : { rows : us_states, select : 'State Select' },
+    tpl : '#product-select-tpl'
+  });
+
+  // $("#product-select").prop("selectedIndex", -1);
+  $('#product-select').on('change', function() {
+    $('input[name="product_id"]').val(this.value);
+  });
+
+  $('#country-select').on('change', function() {
+    $('input[name="state"]').val('');
+    if (this.value == 'United States') {
+      $('#state-select').show();
+      $('input[name="state"]').attr('hidden', true);
+    } else {
+      $('#state-select').hide();
+      $('input[name="state"]').removeAttr('hidden');
     }
   });
+
+  $('#state-select').on('change', function() {
+    $('input[name="state"]').val(this.value);
+  });
+
 
   $form.submit(function(e){
     $loading.fadeIn();
@@ -86,6 +136,8 @@ function initCreateOrders() {
     formData.forEach(function(item){
       submitData[item.name] = (item.name == 'quantity') ? parseInt(item.value) : item.value;
     });
+
+    // console.log(formData);
 
     $.ajax({
       url: '/admin/create-order',
