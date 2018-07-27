6ef1d3c857be0c9e1fc477a88c29a9e8e1737a50
Aymon Nawaz <aymonnawaz123@gmail.com>
2018-07-27 19:24:37 +0500

    layout changes

--- app/routes/developers.js
+++ app/routes/developers.js
@@ -8,8 +8,9 @@ router.get('/guides/get-started', function(req, res, next) {
 });
 
 router.get('/', function(req, res, next) {
-  var filename = path.join(__dirname+'/../views/developers/readme.html');
-  res.sendFile(filename);
+  // var filename = path.join(__dirname+'/../views/developers/readme.html');
+  // res.sendFile(filename);
+  res.render('developers/index');
 });
 
 router.get('/:slug/:slug2?/:slug3?', function(req, res, next) {
@@ -24,18 +25,24 @@ router.get('/:slug/:slug2?/:slug3?', function(req, res, next) {
   }
 
   slugs = slugs.replace('/','');
+  console.log(slugs);
 
   var pathname = json[slugs];
   var filename = '';
 
+  // if (pathname['is_dir']) {
+  //   filename = path.join(__dirname+'/../views/developers/'+slugs+'/readme.html');
+  //   // console.log(filename, pathname['is_dir']);
+  //   res.sendFile(filename);
+  // } else {
+  //   filename = path.join(__dirname+'/../views/developers/'+slugs+'.html');
+  //   // console.log(filename, pathname['is_dir']);
+  //   res.sendFile(filename);
+  // }
   if (pathname['is_dir']) {
-    filename = path.join(__dirname+'/../views/developers/'+slugs+'/readme.html');
-    // console.log(filename, pathname['is_dir']);
-    res.sendFile(filename);
+    res.render('developers/'+slugs+'/readme');
   } else {
-    filename = path.join(__dirname+'/../views/developers/'+slugs+'.html');
-    // console.log(filename, pathname['is_dir']);
-    res.sendFile(filename);
+    res.render('developers/'+slugs);
   }
 });
 
